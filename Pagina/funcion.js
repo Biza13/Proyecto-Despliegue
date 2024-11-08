ba97c8d10389fe09fc5d1bae43f47bd41fb6f814
Bego <111123343+Biza13@users.noreply.github.com>
2024-11-08 20:25:01 +0100

    Update funcion.js
    a

--- Pagina/funcion.js
+++ Pagina/funcion.js
@@ -3,6 +3,9 @@ function sonido(){
     sonido.play();
 }
 
+/**
+*Funcion que reproduce un sonido mp3
+*/
 function sonido2(){
     let sonido = new Audio("./recursos/mugido.mp3");
     sonido.play();
