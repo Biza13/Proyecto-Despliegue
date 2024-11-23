b6ef4f22bff80fea235372497194b64b563f681e
Biza_13 <serrano_0_1@hotmail.com>
2024-11-23 21:14:26 +0100

    archivos de configuracion

--- script-nginxEn-debian.sh
+++ script-nginxEn-debian.sh
@@ -6,4 +6,10 @@ sudo systemctl enable nginx
 sudo apt install php php-fpm -y
 sudo systemctl start php-fpm
 sudo systemctl enable php-fpm
+sudo systemctl enable php8.2-fpm
+sudo systemctl restart php8.2-fpm
 sudo systemctl restart nginx
+sudo apt install rsync
+sudo apt install curl php-cli php-mbstring unzip git -y
+sudo apt install composer
+sudo apt install php8.2-xml
